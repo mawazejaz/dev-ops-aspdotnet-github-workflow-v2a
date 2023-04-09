0142fda70f018fdc550c9d4a29642ce33cf6520f
mawazejaz <mawaz644@gmail.com>
2023-04-09 22:13:33 +0500

    changes

--- MyFirstAzureWebApp/Pages/Index.cshtml.cs
+++ MyFirstAzureWebApp/Pages/Index.cshtml.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Configuration;
 
 namespace MyFirstAzureWebApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    private readonly IConfiguration _configuration;
+    public IndexModel(ILogger<IndexModel> logger,
+        IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public void OnGet()
     {
+        var environment = _configuration["BuildEnvironment:Name"];
+        ViewData["environment"] = environment;
 
     }
     public int Add(int a, int b)
