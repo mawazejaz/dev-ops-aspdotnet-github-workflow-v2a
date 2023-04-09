c85d72038c45b6a21790fa5d2efc8a4ace852309
mawazejaz <mawaz644@gmail.com>
2023-04-09 23:06:40 +0500

    Programe .cs Update

--- MyFirstAzureWebApp/Program.cs
+++ MyFirstAzureWebApp/Program.cs
@@ -1,7 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var isDevelopment = builder.Environment.IsDevelopment();
+
+string environmentName = null;
+#if DEBUG
+environmentName = "Development";
+#elif RELEASE
+environmentName = "Development";
+#elif TESTPUBLISH 
+environmentName = "TestPublish";
+#elif STAGINGPUBLISH 
+environmentName = "StagingPublish";
+#else
+environmentName = "ProductionPublish";
+#endif
+
+builder.Configuration.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
+builder.Configuration.AddEnvironmentVariables();
+// projectwide instances
+
+//builder.Configuration.Get()"DefaultConnection");
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+
 
 var app = builder.Build();
 
